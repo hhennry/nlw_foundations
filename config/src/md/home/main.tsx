65c41bd72fc051ad93c5f157798506372333d0b9
Henry Rodrigues <108133435+hhennry@users.noreply.github.com>
2023-09-12 10:20:58 -0300

    Initial commit

---
+++ config/src/md/home/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('home') as HTMLElement).render(
+  <React.StrictMode>
+    {/* NameImport */}
+  </React.StrictMode>,
+)
