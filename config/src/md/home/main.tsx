8ff5fef540e250c83640481f9563dba976e8e8f4
Henry Rodrigues <108133435+hhennry@users.noreply.github.com>
2023-10-05 23:27:17 +0000

    Pending changes exported from your codespace

--- config/src/md/home/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-ReactDOM.createRoot(document.getElementById('home') as HTMLElement).render(
-  <React.StrictMode>
-    {/* NameImport */}
-  </React.StrictMode>,
-)
